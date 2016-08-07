57970bc4ba12023dc7de95a95882c4dd68d9671f
pangratz <cmueller.418@gmail.com>
2016-08-07 14:03:04 +0200

    Cleanup patch for AdapterPopulatedRecordArray
    Reference setup is already done in RecordArray.

--- addon/patches/adapter-populated-record-array.js
+++ addon/patches/adapter-populated-record-array.js
@@ -4,21 +4,12 @@ const { AdapterPopulatedRecordArray } = DS;
 
 AdapterPopulatedRecordArray.reopen({
 
-  init() {
-    this._super(...arguments);
-
-    this._ref = new RecordArrayReference({ recordArray: this });
-  },
-
-  ref() {
-    return this._ref;
-  },
-
   loadRecords(records, payload) {
     this._super(...arguments);
 
-    this._ref.__update_meta(payload.meta);
-    this._ref.__update_links(payload.links);
+    let ref = this.ref();
+    ref.__update_meta(payload.meta);
+    ref.__update_links(payload.links);
   }
 
 });

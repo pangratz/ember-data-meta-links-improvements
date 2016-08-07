586e81afe618f824dc3757fd44b9c64466bc7ec4
pangratz <cmueller.418@gmail.com>
2016-08-07 14:54:20 +0200

    Implement initializer so Ember Data is patched when app is loaded

---
+++ addon/initializers/ember-data-meta-links-improvements.js
@@ -0,0 +1,10 @@
+import "ember-data-meta-links-improvements"
+
+export function initialize(/* application */) {
+  // patches are applied by importing ember-data-meta-links-improvements
+}
+
+export default {
+  name: 'ember-data-meta-links-improvements',
+  initialize
+};
---
+++ app/initializers/ember-data-meta-links-improvements.js
@@ -0,0 +1,1 @@
+export { default, initialize } from 'ember-data-meta-links-improvements/initializers/ember-data-meta-links-improvements';
